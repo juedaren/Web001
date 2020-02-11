b6ffd0c5f6d0fd5c330bd597e0111591b337b2d7
路文 <1972293086@qq.com>
2020-02-11 20:42:35 +0800

    Initial commit

---
+++ src/com/atguigui/bean/Student.java
@@ -0,0 +1,37 @@
+package com.atguigui.bean;
+
+/**
+ * @author lemon
+ * @create 2020-02-11 19:14
+ */
+public class Student {
+
+    private Integer id;
+    private String name;
+
+    public void Student(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
